4b19fe783f133a6d4c5b64d56b59f14001d5a520
Volodymyr Slipchyk <vova.slipchyk@gmail.com>
2026-05-19 17:32:26 +0300

    clear

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,6 @@ import com.leetcode.problems.Solution;
 
 public class Main {
     public static void main(String[] args) {
-//        Solution solution = new Solution();
-//        System.out.println(solution.isSubsequence("ace", "abcde"));
-
         SlidingWindowProblem slwp = new SlidingWindowProblem();
         slwp.getWindowsLessThen10(new int[] {3,2,1,3,1,1}, 6);
     }
